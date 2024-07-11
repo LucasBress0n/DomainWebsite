4f816e5d382883b36682814f1cc95f9c0d734f09
Lucas Bresson <pandaboy816@gmail.com>
2024-07-11 02:14:18 -0500

    message pls work

---
+++ main.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./src/App.jsx";
+ReactDOM.render( /*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(App, null)), document.getElementById("root"));
