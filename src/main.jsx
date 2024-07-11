7fbb7bc0709991ce80533d5ecb16e5b7a71531c4
Lucas Bresson <pandaboy816@gmail.com>
2024-07-11 01:18:09 -0500

    changes1

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,4 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
